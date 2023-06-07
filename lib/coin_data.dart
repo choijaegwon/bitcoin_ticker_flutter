fbbc701aa94a7f4ccc31627dcee6519a7bbdec0f
choijaegwon <chl9338@naver.com>
2023-06-08 00:13:08 +0900

    first commit

---
+++ lib/coin_data.dart
@@ -0,0 +1,56 @@
+import 'package:http/http.dart' as http;
+import 'apiKey.dart';
+import 'dart:convert';
+
+const List<String> currenciesList = [
+  'AUD',
+  'BRL',
+  'CAD',
+  'CNY',
+  'EUR',
+  'GBP',
+  'HKD',
+  'IDR',
+  'ILS',
+  'INR',
+  'JPY',
+  'MXN',
+  'NOK',
+  'NZD',
+  'PLN',
+  'RON',
+  'RUB',
+  'SEK',
+  'SGD',
+  'USD',
+  'ZAR'
+];
+
+const List<String> cryptoList = [
+  'BTC',
+  'ETH',
+  'LTC',
+];
+
+const URL = 'rest.coinapi.io';
+const Path = 'v1/exchangerate';
+
+class CoinData {
+  Future getCoinData(String selectedCurrency) async {
+    Map<String, String> cryptoPrices = {};
+    for (String crypto in cryptoList) {
+      Uri uri = Uri.https(
+          '$URL', '$Path/$crypto/$selectedCurrency', {'apikey': '$apiKey'});
+      http.Response response = await http.get(uri);
+
+      if (response.statusCode == 200) {
+        var decodedData = jsonDecode(response.body);
+        double lastPrice = decodedData['rate'];
+        cryptoPrices[crypto] = lastPrice.toStringAsFixed(0);
+      } else {
+        print(response.statusCode);
+      }
+    }
+    return cryptoPrices;
+  }
+}
